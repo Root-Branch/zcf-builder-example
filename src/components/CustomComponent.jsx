d75d4104e78c588154b8b5de7933cd7294256a49
ohuu <oliver@paperplane.software>
2024-12-09 17:25:11 +0000

    stuff and fings

---
+++ src/components/CustomComponent.jsx
@@ -0,0 +1,9 @@
+// src/components/CustomComponent.jsx
+import React from 'react';
+import { Builder } from '@builder.io/react';
+
+const CustomComponent = ({ text }) => {
+  return <div className="custom-component">{text}</div>;
+};
+
+export default CustomComponent;
